320bdbf224228af0c582ea37cc008d3b47f9e1ad
Maria Eduarda de oliveira oti <maria.oti@escola.pr.gov.br>
2026-03-31 12:36:14 -0300

    31/03/2026

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(600, 400);
+  background(255); // fundo branco
+  
+  // Círculo vermelho no centro
+  noStroke();
+  fill(215, 0, 58); // Cor vermelha
+  ellipse(width / 2, height / 2, 240, 240);
+}
